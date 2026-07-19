c2bf01a626362e661656c174217b60e6f5856143
manufosela <manufosela@gmail.com>
2026-07-19 11:51:53 +0200

    test: verify Node<->browser interoperability with Playwright

---
+++ playwright.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'e2e',
+  testMatch: '**/*.spec.mjs',
+  use: { channel: 'chrome', headless: true },
+});
